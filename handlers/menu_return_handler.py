be49fd9900f4278c5c07123bec1963499b863c09
George <george.bobovsky@gmail.com>
2025-12-28 12:37:48 +0500

    f-2

--- handlers/menu_return_handler.py
+++ handlers/menu_return_handler.py
@@ -2,15 +2,13 @@ from bot_instance import bot
 from keyboards import menu_keyboard
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'main_menu')
+@bot.callback_query_handler(func=lambda call: call.data == "main_menu")
 def menu_return_handler(call):
     bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.id)
 
     bot.send_message(
         chat_id=call.message.chat.id,
-        text='🔙 <b>Возврат в меню</b>\n\n'
-             '<i>Куда отправимся?</i>\n'
-             '↓ ↓ ↓',
-        parse_mode='html',
-        reply_markup=menu_keyboard
+        text="🔙 <b>Возврат в меню</b>\n\n<i>Куда отправимся?</i>\n↓ ↓ ↓",
+        parse_mode="html",
+        reply_markup=menu_keyboard,
     )
